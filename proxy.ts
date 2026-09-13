f0c831b1caff44468ded7165f935d8cc2e555d18
Aman <skylerop9999@email.com>
2026-09-13 23:38:22 +0530

    chnage hometab mobile freindly

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,8 @@ import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware({
   frontendApiProxy: {
-    enabled: process.env.NODE_ENV === "production",
+    // enabled: process.env.NODE_ENV === "production",
+    enabled: true,
   },
 })
 
